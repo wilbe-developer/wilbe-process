309130d0704ec7adf4ba868333c15466b10f9ace
wilbe-developer <dev@wilbe.com>
2025-04-08 15:26:55 +0100

    Route PlasmicLandingPageOld
    Routing a plasmic page to appear in the app

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import PlasmicLandingPage from "@/components/plasmic/wilbe_com/PlasmicLandingPageOld";
+
+export default function LandingPage() {
+  return <PlasmicLandingPage />;
+}
