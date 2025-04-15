abf4c267fefdda4dc24c43973fa30575cc52efb6
wilbe-developer <dev@wilbe.com>
2025-04-15 13:53:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -11,8 +12,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    svgr(), // ✅ added here
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
